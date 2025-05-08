29ec8371ce43ea60f3b9b8e77efbcae6e5372ecf
Wallace <wallacecicero3280@gmail.com>
2025-05-08 18:50:35 -0300

    second commit

---
+++ Test01.py
@@ -0,0 +1,5 @@
+day = input('Digite o Dia que voce nasceu: ')
+month = input('Digite o Mes que voce nasceu: ')
+year = input('Digite o Ano que voce nasceu: ')
+year_old = 2020 - int(year)
+print(f'Voce nasceu no dia {day} do mes {month} do ano {year}. Voce tem {year_old} anos.')
